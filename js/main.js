4c2b4b57fdab7a63c5dcef1bc79eb73cdb062a9b
Hablu-Programmer <habluprogrammer@gmail.com>
2022-09-14 10:40:20 +0600

    ddd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const slider = document.querySelector('.slider input');
-const img = document.querySelector('.images .img-2');
-const dragLine = document.querySelector('.slider .drag-line');
-slider.oninput = () => {
-	let sliderVal = slider.value;
-	dragLine.style.left = sliderVal + '%';
-	img.style.width = sliderVal + '%';
-};
+// const slider = document.querySelector('.slider input');
+// const img = document.querySelector('.images .img-2');
+// const dragLine = document.querySelector('.slider .drag-line');
+// slider.oninput = () => {
+// 	let sliderVal = slider.value;
+// 	dragLine.style.left = sliderVal + '%';
+// 	img.style.width = sliderVal + '%';
+// };
